94001afbafe667dc54b6c8a3c21824f9fff07642
Jon Warren <jon@elementalpower.co.uk>
2024-01-11 18:30:50 +0000

    Add test project - with a single test to check the output is unchanged for known results

---
+++ epoch_simulator/EP_Test/test.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+
+#include <filesystem>
+#include <fstream>
+
+#include "../EP/io/FileConfig.h";
+#include "../EP/Optimisation/Optimiser.hpp"
+#include "../EP/io/FileHandling.hpp"
+
+namespace fs = std::filesystem;
+
+TEST(EPTestCase, MatchesKnownOutput) {
+
+	FileConfig fileConfig = FileConfig{
+		"", "KnownInput", "OutputData",
+		"CSVEload.csv", "CSVHload.csv", "CSVRGen.csv",
+		"TestResults.csv", "TestOutputParameters.json", "TestOuputParametersFromInit.json"
+	};
+
+	Optimiser opt = Optimiser(fileConfig);
+
+	// Run the Optimiser on known input
+	// TODO defaultInput -> knownInput
+	auto converted_json = handleJsonConversion(defaultInput, fileConfig.getInputDir());
+	OutputValues testOutput = opt.runMainOptimisation(converted_json);
+	auto testJson = outputToJson(testOutput);
+	writeJsonToFile(testJson, fileConfig.getOutputJsonFilepath());
+
+	//// Load the known output
+	fs::path knownOutputFile = fs::path{ "KnownOutput" } / fs::path{ "KnownOutput.json" };
+
+
+	std::ifstream f(knownOutputFile);
+	auto knownJson = nlohmann::json::parse(f);
+
+
+	EXPECT_EQ(testJson["CAPEX"], knownJson["CAPEX"]);
+	EXPECT_EQ(testJson["CAPEX_index"], knownJson["CAPEX_index"]);
+
+	EXPECT_EQ(testJson["annualised"], knownJson["annualised"]);
+	EXPECT_EQ(testJson["annualised_index"], knownJson["annualised_index"]);
+
+	EXPECT_EQ(testJson["scenario_cost_balance"], knownJson["scenario_cost_balance"]);
+	EXPECT_EQ(testJson["scenario_cost_balance_index"], knownJson["scenario_cost_balance_index"]);
+
+	EXPECT_EQ(testJson["payback_horizon"], knownJson["payback_horizon"]);
+	EXPECT_EQ(testJson["payback_horizon_index"], knownJson["payback_horizon_index"]);
+
+	EXPECT_EQ(testJson["scenario_carbon_balance"], knownJson["scenario_carbon_balance"]);
+	EXPECT_EQ(testJson["scenario_carbon_balance_index"], knownJson["scenario_carbon_balance_index"]);
+
+}
