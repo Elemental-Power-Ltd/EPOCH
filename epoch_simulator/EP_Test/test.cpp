74b33f6c7e9192f30186eb022882350e709c905e
Jon Warren <jon@elementalpower.co.uk>
2024-01-18 19:41:00 +0000

    Change test to only check values - as best index is semi-random when multiple scenarios produce the same result

--- epoch_simulator/EP_Test/test.cpp
+++ epoch_simulator/EP_Test/test.cpp
@@ -35,18 +35,18 @@ TEST(EPTestCase, MatchesKnownOutput) {
 
 
 	EXPECT_EQ(testJson["CAPEX"], knownJson["CAPEX"]);
-	EXPECT_EQ(testJson["CAPEX_index"], knownJson["CAPEX_index"]);
+	//EXPECT_EQ(testJson["CAPEX_index"], knownJson["CAPEX_index"]);
 
 	EXPECT_EQ(testJson["annualised"], knownJson["annualised"]);
-	EXPECT_EQ(testJson["annualised_index"], knownJson["annualised_index"]);
+	//EXPECT_EQ(testJson["annualised_index"], knownJson["annualised_index"]);
 
 	EXPECT_EQ(testJson["scenario_cost_balance"], knownJson["scenario_cost_balance"]);
-	EXPECT_EQ(testJson["scenario_cost_balance_index"], knownJson["scenario_cost_balance_index"]);
+	//EXPECT_EQ(testJson["scenario_cost_balance_index"], knownJson["scenario_cost_balance_index"]);
 
 	EXPECT_EQ(testJson["payback_horizon"], knownJson["payback_horizon"]);
-	EXPECT_EQ(testJson["payback_horizon_index"], knownJson["payback_horizon_index"]);
+	//EXPECT_EQ(testJson["payback_horizon_index"], knownJson["payback_horizon_index"]);
 
 	EXPECT_EQ(testJson["scenario_carbon_balance"], knownJson["scenario_carbon_balance"]);
-	EXPECT_EQ(testJson["scenario_carbon_balance_index"], knownJson["scenario_carbon_balance_index"]);
+	//EXPECT_EQ(testJson["scenario_carbon_balance_index"], knownJson["scenario_carbon_balance_index"]);
 
 }
