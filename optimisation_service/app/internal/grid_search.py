d3a9d8f40683a9396bc45bcd97e808c857a0a80f
William-EPL <william@elementalpower.co.uk>
2024-08-08 14:44:11 +0100

    simplify grid search

--- optimisation_service/app/internal/grid_search.py
+++ optimisation_service/app/internal/grid_search.py
@@ -3,7 +3,6 @@ import os
 import tempfile
 import time
 from datetime import timedelta
-from os import PathLike
 from pathlib import Path
 
 import numpy as np
@@ -14,7 +13,6 @@ from .opt_algorithm import Algorithm
 from .problem import _OBJECTIVES, _OBJECTIVES_DIRECTION, Problem, convert_param
 from .result import Result
 from .task_data_wrapper import run_headless
-from .utils import typename
 
 _EPOCH_CONFIG = {"optimiser": {"leagueTableCapacity": 1, "produceExhaustiveOutput": True}}
 
@@ -26,9 +24,6 @@ class GridSearch(Algorithm):
 
     def __init__(
         self,
-        project_path: str | PathLike | None = None,
-        output_dir: str | PathLike | None = None,
-        config_dir: str | PathLike | None = None,
         keep_degenerate: bool = False,
     ) -> None:
         """
@@ -36,22 +31,9 @@ class GridSearch(Algorithm):
 
         Parameters
         ----------
-        output_dir
-            Path to directory to save outputs of EPOCH.
-            Defaults to a temporary directory that is deleted after completion of optimisation.
-        project_path
-            Path to EPOCH directory
-        config_dir
-            Path to EPOCH config directory
-            Defaults to a temporary directory with the necessary config file that is deleted after completion of optimisation.
         keep_degenerate
             Whether or not to keep degenerate solutions in solutions
         """
-        if project_path is None:
-            project_path = os.environ.get("EPOCH_DIR", "../EPOCH")
-        self.project_path = project_path
-        self.config_dir = config_dir
-        self.output_dir = output_dir
         self.keep_degenerate = keep_degenerate
 
     async def run(self, problem: Problem) -> Result:
@@ -70,31 +52,24 @@ class GridSearch(Algorithm):
         objective_values
             objective_values of optimal solutions.
         """
-        if self.output_dir is None or self.config_dir is None:
-            has_temp_dir = True
-            temp_dir = tempfile.TemporaryDirectory()
-        else:
-            has_temp_dir = False
+        temp_dir = tempfile.TemporaryDirectory()
 
-        if self.output_dir is None:
-            output_dir = Path(temp_dir.name, "tmp_outputs")
-        else:
-            output_dir = Path(self.output_dir, typename(self))
+        output_dir = Path(temp_dir.name, "tmp_outputs")
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        if self.config_dir is None:
-            config_dir = Path(temp_dir.name, "Config")
-            Path(temp_dir.name, "Config").mkdir(parents=False, exist_ok=False)
-            with open(Path(config_dir, "EpochConfig.json"), "w") as f:
-                json.dump(_EPOCH_CONFIG, f)
+        config_dir = Path(temp_dir.name, "Config")
+        Path(temp_dir.name, "Config").mkdir(parents=False, exist_ok=False)
+
+        with open(Path(config_dir, "EpochConfig.json"), "w") as f:
+            json.dump(_EPOCH_CONFIG, f)
 
         with open(Path(problem.input_dir, "inputParameters.json"), "w") as f:
             json.dump(convert_param(problem.parameters), f)
 
         t0 = time.perf_counter()
         await run_headless(
-            project_path=str(self.project_path),
+            project_path=str(os.environ.get("EPOCH_DIR", "../Epoch")),
             config_dir=str(config_dir),
             input_dir=str(problem.input_dir),
             output_dir=str(output_dir),
@@ -118,7 +93,6 @@ class GridSearch(Algorithm):
         solutions = solutions[pareto_efficient]
         objective_values = df_res[_OBJECTIVES].to_numpy()[pareto_efficient]
 
-        if has_temp_dir:
-            temp_dir.cleanup()
+        temp_dir.cleanup()
 
         return Result(solutions=solutions, objective_values=objective_values, exec_time=exec_time, n_evals=problem.size())
