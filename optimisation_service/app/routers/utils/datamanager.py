67fb5e9379813069f2b7e8097c3b342024eef75c
William-EPL <william@elementalpower.co.uk>
2024-08-21 16:40:03 +0100

    test submit-task success

--- optimisation_service/app/routers/utils/datamanager.py
+++ optimisation_service/app/routers/utils/datamanager.py
@@ -84,9 +84,9 @@ class DataManager:
         """
         logger.debug(f"Fetching site data info {site_data_id}.")
         async with httpx.AsyncClient() as client:
-            return await self.db_post(client=client, subdirectory="/get-client-site-data", data={"input_data_ID": site_data_id})
+            return await self.db_post(client=client, subdirectory="/list-latest-datasets", data={"input_data_ID": site_data_id})
 
-    async def fetch_all_input_data(self, site_data_ids: dict[str, DatasetIDWithTime]) -> dict[str, pd.DataFrame]:
+    async def fetch_all_input_data(self, site_data_ids: dict[str, dict]) -> dict[str, pd.DataFrame]:
         """
         Fetch and process all necessary input data from database.
 
@@ -102,14 +102,30 @@ class DataManager:
         site_data = {}
         async with httpx.AsyncClient() as client:
             async with asyncio.TaskGroup() as tg:
-                Eload_task = tg.create_task(self.fetch_electricity_data(site_data_ids["electricity_dataset"], client))
-                Hload_task = tg.create_task(self.fetch_heat_data(site_data_ids["gas_dataset"], client))
-                Airtemp_task = tg.create_task(self.fetch_airtemp_data(site_data_ids["gas_dataset"], client))
-                RGen_task = tg.create_task(self.fetch_rgen_data(site_data_ids["rgen_dataset"], client))
-                ASHPinput_task = tg.create_task(self.fetch_ASHP_input_data(site_data_ids["ashp_input_dataset"], client))
-                ASHPoutput_task = tg.create_task(self.fetch_ASHP_output_data(site_data_ids["ashp_output_dataset"], client))
-                Importtariff_task = tg.create_task(self.fetch_import_tariff_data(site_data_ids["tariff_dataset"], client))
-                GridCO2_task = tg.create_task(self.fetch_grid_CO2_data(site_data_ids["grid_CO2_dataset"], client))
+                Eload_task = tg.create_task(
+                    self.fetch_electricity_data({"dataset_id": site_data_ids["ElectricityMeterData"]["dataset_id"]}, client)
+                )
+                Hload_task = tg.create_task(
+                    self.fetch_heat_data({"dataset_id": site_data_ids["HeatingLoad"]["dataset_id"]}, client)
+                )
+                Airtemp_task = tg.create_task(
+                    self.fetch_airtemp_data({"dataset_id": site_data_ids["HeatingLoad"]["dataset_id"]}, client)
+                )
+                RGen_task = tg.create_task(
+                    self.fetch_rgen_data({"dataset_id": site_data_ids["RenewablesGeneration"]["dataset_id"]}, client)
+                )
+                ASHPinput_task = tg.create_task(
+                    self.fetch_ASHP_input_data({"dataset_id": site_data_ids["ASHPData"]["dataset_id"]}, client)
+                )
+                ASHPoutput_task = tg.create_task(
+                    self.fetch_ASHP_output_data({"dataset_id": site_data_ids["ASHPData"]["dataset_id"]}, client)
+                )
+                Importtariff_task = tg.create_task(
+                    self.fetch_import_tariff_data({"dataset_id": site_data_ids["ImportTariff"]["dataset_id"]}, client)
+                )
+                GridCO2_task = tg.create_task(
+                    self.fetch_grid_CO2_data({"dataset_id": site_data_ids["CarbonIntensity"]["dataset_id"]}, client)
+                )
         site_data["Eload"] = Eload_task.result()
         site_data["Hload"] = Hload_task.result()
         site_data["Airtemp"] = Airtemp_task.result()
--- optimisation_service/tests/endpoints/test_app_main.py
+++ optimisation_service/tests/endpoints/test_app_main.py
@@ -5,6 +5,9 @@ from app.main import app
 
 class TestMain:
     def test_read_main(self):
+        """
+        Test server.
+        """
         with TestClient(app) as client:
             response = client.get("/")
             assert response.status_code == 200
