4dc4bc3a306473fb037a29911b60255dd58fa085
William-EPL <william@elementalpower.co.uk>
2024-10-24 17:07:37 +0100

    only load and calc necessary data

--- optimisation_service/app/internal/heuristics/population_init.py
+++ optimisation_service/app/internal/heuristics/population_init.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from os import PathLike
 from pathlib import Path
 
@@ -16,6 +17,15 @@ from .population_heuristics import (
     estimate_solar_pv,
     round_to_search_space,
 )
+
+
+class LazyDict(dict):
+    def __getitem__(self, item):
+        value = dict.__getitem__(self, item)
+        if callable(value):
+            value = value()
+            dict.__setitem__(self, item, value)
+        return value
 
 
 def generate_initial_population(
@@ -37,48 +47,55 @@ def generate_initial_population(
     pop_size
         number of solutions generated in population.
     """
-    heating_df = pd.read_csv(Path(input_dir, "CSVHload.csv"))
-    ashp_input_df = pd.read_csv(Path(input_dir, "CSVASHPinput.csv"))
-    ashp_output_df = pd.read_csv(Path(input_dir, "CSVASHPoutput.csv"))
-    air_temp_df = pd.read_csv(Path(input_dir, "CSVAirtemp.csv"))
-    elec_df = pd.read_csv(Path(input_dir, "CSVEload.csv"))
-    solar_df = pd.read_csv(Path(input_dir, "CSVRGen.csv"))
+    dfs = LazyDict()
+    dfs["heating_df"] = lambda: pd.read_csv(Path(input_dir, "CSVHload.csv"))
+    dfs["ashp_input_df"] = lambda: pd.read_csv(Path(input_dir, "CSVASHPinput.csv"))
+    dfs["ashp_output_df"] = lambda: pd.read_csv(Path(input_dir, "CSVASHPoutput.csv"))
+    dfs["air_temp_df"] = lambda: pd.read_csv(Path(input_dir, "CSVAirtemp.csv"))
+    dfs["elec_df"] = lambda: pd.read_csv(Path(input_dir, "CSVEload.csv"))
+    dfs["solar_df"] = lambda: pd.read_csv(Path(input_dir, "CSVRGen.csv"))
+
+    estimates = LazyDict()
+    estimates["ASHP_HPower"] = lambda: estimate_ashp_hpower(
+        heating_df=dfs["heating_df"],
+        ashp_input_df=dfs["ashp_input_df"],
+        ashp_output_df=dfs["ashp_output_df"],
+        air_temp_df=dfs["air_temp_df"],
+        ashp_mode=constant_param["ASHP_HSource"],
+    )
+    estimates["ESS_capacity"] = lambda: estimate_battery_capacity(elec_df=dfs["elec_df"])
+    estimates["ScalarRG1"] = lambda: estimate_solar_pv(solar_df=dfs["solar_df"], elec_df=dfs["elec_df"])
+    estimates["ESS_charge_power"] = lambda: estimate_battery_charge(
+        solar_df=dfs["solar_df"], solar_scale=estimates["ScalarRG1"]
+    )
+    estimates["ESS_discharge_power"] = lambda: estimate_battery_discharge(elec_df=dfs["elec_df"])
+
+    rng = np.random.default_rng()
+
+    def clipped_rand(lo, hi, step):
+        x = rng.choice(a=np.arange(lo, hi), size=pop_size)
+        return round_to_search_space(x, lo, hi, step)
+
+    def clipped_norm(est, lo, hi, step):
+        sigma = np.abs(hi - lo) / 4
+        a = (lo - est) / sigma
+        b = (hi - est) / sigma
+        x = np.clip(truncnorm.rvs(a=a, b=b, loc=est, scale=sigma, size=pop_size), lo, hi)
+        return round_to_search_space(x, lo, hi, step)
+
+    sampler_funcs = defaultdict(lambda: lambda lo, hi, step: clipped_rand(lo, hi, step))
+    sampler_funcs["ASHP_HPower"] = lambda lo, hi, step: clipped_norm(estimates["ASHP_HPower"], lo, hi, step)
+    sampler_funcs["ESS_capacity"] = lambda lo, hi, step: clipped_norm(estimates["ESS_capacity"], lo, hi, step)
+    sampler_funcs["ESS_charge_power"] = lambda lo, hi, step: clipped_norm(estimates["ESS_charge_power"], lo, hi, step)
+    sampler_funcs["ESS_discharge_power"] = lambda lo, hi, step: clipped_norm(estimates["ESS_discharge_power"], lo, hi, step)
+    sampler_funcs["ScalarRG1"] = lambda lo, hi, step: clipped_norm(estimates["ScalarRG1"], lo, hi, step)
 
     pop, lbs, steps = [], np.array([]), np.array([])
-    estimatable = ["ASHP_HPower", "ESS_capacity", "ESS_charge_power", "ESS_discharge_power", "ScalarRG1"]
     for parameter, param_range in variable_param.items():
-        lower, upper, step = param_range["min"], param_range["max"], param_range["step"]
-        if parameter in estimatable:
-            if parameter == "ASHP_HPower":
-                ashp_mode = constant_param["ASHP_HSource"]
-                estimate = estimate_ashp_hpower(
-                    heating_df=heating_df,
-                    ashp_input_df=ashp_input_df,
-                    ashp_output_df=ashp_output_df,
-                    air_temp_df=air_temp_df,
-                    ashp_mode=ashp_mode,
-                )
-            if parameter == "ESS_capacity":
-                estimate = estimate_battery_capacity(elec_df=elec_df)
-            if parameter == "ESS_charge_power":
-                solar_scale = estimate_solar_pv(solar_df=solar_df, elec_df=elec_df)
-                estimate = estimate_battery_charge(solar_df=solar_df, solar_scale=solar_scale)
-            if parameter == "ESS_discharge_power":
-                estimate = estimate_battery_discharge(elec_df=elec_df)
-            if parameter == "ScalarRG1":
-                estimate = estimate_solar_pv(solar_df=solar_df, elec_df=elec_df)
-
-            sigma = (upper - lower) / 4
-            a = (lower - estimate) / sigma
-            b = (upper - estimate) / sigma
-            non_clipped_values = truncnorm.rvs(a=a, b=b, loc=estimate, scale=sigma, size=pop_size)
-            generated_values = round_to_search_space(x=non_clipped_values, start=lower, stop=upper, step=step)
-        else:
-            possible_values = np.arange(lower, upper + step, step)
-            generated_values = np.random.choice(possible_values, size=pop_size)
-
+        lo, hi, step = param_range["min"], param_range["max"], param_range["step"]
+        generated_values = sampler_funcs[parameter](lo, hi, step)
         pop.append(generated_values)
-        lbs, steps = np.append(lbs, lower), np.append(steps, step)
+        lbs, steps = np.append(lbs, lo), np.append(steps, step)
     pop = np.array(pop)
     pop = pop.transpose()
     scaled_pop = (pop - lbs) / steps
