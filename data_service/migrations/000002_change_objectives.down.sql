17b9b516e243607fd0c0601edda8d7a37953a391
Matt Bailey <32373320+Matt-HJ-Bailey@users.noreply.github.com>
2025-01-22 16:11:44 +0000

    Added migrations for new database format for results, and made docker container image read migrations

--- data_service/migrations/000002_change_objectives.down.sql
+++ data_service/migrations/000002_change_objectives.down.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 ALTER TYPE public.objective_t RENAME ATTRIBUTE carbon_balance_scope_1 TO carbon_balance CASCADE;
-ALTER TYPE public.objective_t DROP ATTRIBUTE carbon_balance_scope_2 IF EXISTS CASCADE;
-ALTER TYPE public.objective_t DROP ATTRIBUTE carbon_cost IF EXISTS CASCADE;
+ALTER TYPE public.objective_t DROP ATTRIBUTE carbon_balance_scope_2 CASCADE;
+ALTER TYPE public.objective_t DROP ATTRIBUTE carbon_cost CASCADE;
 
 END;
