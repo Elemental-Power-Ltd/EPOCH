9afb4e5edf82fabd526b9d90396193955ea9d7f8
William-EPL <william@elementalpower.co.uk>
2025-01-31 10:00:52 +0000

    fix merge error

--- optimisation_service/app/models/result.py
+++ optimisation_service/app/models/result.py
@@ -1,28 +1,8 @@
 from dataclasses import dataclass
 from datetime import timedelta
 
-<<<<<<< HEAD
-import numpy as np
-
-from app.internal.epoch_utils import SimulationResult, TaskData
-from app.models.objectives import _OBJECTIVES, Objectives, ObjectiveValues
-
-
-def convert_sim_result(sim_result: SimulationResult) -> ObjectiveValues:
-    objective_values = ObjectiveValues()
-    for objective in _OBJECTIVES:
-        if objective == Objectives.carbon_cost:
-            if sim_result.carbon_balance_scope_1 > 0:
-                objective_values[Objectives.carbon_cost] = sim_result.capex / (sim_result.carbon_balance_scope_1 * 15 / 1000)
-            else:
-                objective_values[Objectives.carbon_cost] = np.finfo(np.float32).max
-        else:
-            objective_values[objective] = getattr(sim_result, objective)
-    return objective_values
-=======
 from app.internal.epoch_utils import TaskData
 from app.models.objectives import ObjectiveValues
->>>>>>> 8ac6f7d5594df344b18d6aad2289ecaa89634e79
 
 
 @dataclass
