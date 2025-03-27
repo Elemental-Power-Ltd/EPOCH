3dc73490a32861f0d90e184db0ad8cd41d1d8548
Matt Bailey <32373320+Matt-HJ-Bailey@users.noreply.github.com>
2025-03-27 14:25:45 +0000

    Fixed SQL lint complaints

--- data_service/migrations/000006_add_total_metrics.down.sql
+++ data_service/migrations/000006_add_total_metrics.down.sql
@@ -1,25 +1,25 @@
 BEGIN;
 
 ALTER TABLE optimisation.portfolio_results
-    DROP COLUMN IF EXISTS metric_total_gas_used,
-    DROP COLUMN IF EXISTS metric_total_electricity_imported,
-    DROP COLUMN IF EXISTS metric_total_electricity_generated,
-    DROP COLUMN IF EXISTS metric_total_electricity_exported,
-    DROP COLUMN IF EXISTS metric_total_electrical_shortfall,
-    DROP COLUMN IF EXISTS metric_total_heat_shortfall,
-    DROP COLUMN IF EXISTS metric_total_gas_import_cost,
-    DROP COLUMN IF EXISTS metric_total_electricity_import_cost,
-    DROP COLUMN IF EXISTS metric_total_electricity_export_gain;
+DROP COLUMN IF EXISTS metric_total_gas_used,
+DROP COLUMN IF EXISTS metric_total_electricity_imported,
+DROP COLUMN IF EXISTS metric_total_electricity_generated,
+DROP COLUMN IF EXISTS metric_total_electricity_exported,
+DROP COLUMN IF EXISTS metric_total_electrical_shortfall,
+DROP COLUMN IF EXISTS metric_total_heat_shortfall,
+DROP COLUMN IF EXISTS metric_total_gas_import_cost,
+DROP COLUMN IF EXISTS metric_total_electricity_import_cost,
+DROP COLUMN IF EXISTS metric_total_electricity_export_gain;
 
 ALTER TABLE optimisation.site_results
-    DROP COLUMN IF EXISTS metric_total_gas_used,
-    DROP COLUMN IF EXISTS metric_total_electricity_imported,
-    DROP COLUMN IF EXISTS metric_total_electricity_generated,
-    DROP COLUMN IF EXISTS metric_total_electricity_exported,
-    DROP COLUMN IF EXISTS metric_total_electrical_shortfall,
-    DROP COLUMN IF EXISTS metric_total_heat_shortfall,
-    DROP COLUMN IF EXISTS metric_total_gas_import_cost,
-    DROP COLUMN IF EXISTS metric_total_electricity_import_cost,
-    DROP COLUMN IF EXISTS metric_total_electricity_export_gain;
+DROP COLUMN IF EXISTS metric_total_gas_used,
+DROP COLUMN IF EXISTS metric_total_electricity_imported,
+DROP COLUMN IF EXISTS metric_total_electricity_generated,
+DROP COLUMN IF EXISTS metric_total_electricity_exported,
+DROP COLUMN IF EXISTS metric_total_electrical_shortfall,
+DROP COLUMN IF EXISTS metric_total_heat_shortfall,
+DROP COLUMN IF EXISTS metric_total_gas_import_cost,
+DROP COLUMN IF EXISTS metric_total_electricity_import_cost,
+DROP COLUMN IF EXISTS metric_total_electricity_export_gain;
 
 END;
