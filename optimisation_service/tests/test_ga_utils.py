a651c0be7186796dd3456a6509d561c562afd61f
William Drouard <william@elementalpower.co.uk>
2025-04-29 09:21:32 +0100

    add ga conversion unit test

--- optimisation_service/tests/test_ga_utils.py
+++ optimisation_service/tests/test_ga_utils.py
@@ -41,6 +41,14 @@ class TestProblemInstance:
                     assert asset_name in td_dict.keys()
 
     @pytest.mark.parametrize("x_value", [0, 1])
+    def test_convert_site_scenario_to_chromosome(self, x_value: int, default_problem_instance: ProblemInstance):
+        for site in default_problem_instance.portfolio:
+            x = np.array([x_value] * count_parameters_to_optimise(site.site_range))
+            site_scenario = default_problem_instance.convert_solution(x, site.name)
+            chromosome = default_problem_instance.convert_site_scenario_to_chromosome(site_scenario, site.name)
+            assert all(chromosome == x)
+
+    @pytest.mark.parametrize("x_value", [0, 1])
     def test_simulate_portfolio(self, x_value: int, default_problem_instance: ProblemInstance) -> None:
         portfolio = default_problem_instance.portfolio
         x = np.array([x_value] * sum(count_parameters_to_optimise(site.site_range) for site in portfolio))
