20383070267aa2048b4baf996eef5bc6d24116f7
William Drouard <william@elementalpower.co.uk>
2025-05-14 11:25:30 +0100

    fix linting

--- data_service/migrations/000008_add_optimisers.down.sql
+++ data_service/migrations/000008_add_optimisers.down.sql
@@ -1,7 +1,12 @@
 BEGIN;
 
-DELETE FROM optimisation.optimisers WHERE name = 'Bayesian';
-DELETE FROM optimisation.optimisers WHERE name = 'SeparatedNSGA2';
-DELETE FROM optimisation.optimisers WHERE name = 'SeparatedNSGA2xNSGA2';
+DELETE FROM optimisation.optimisers
+WHERE name = 'Bayesian';
+
+DELETE FROM optimisation.optimisers
+WHERE name = 'SeparatedNSGA2';
+
+DELETE FROM optimisation.optimisers
+WHERE name = 'SeparatedNSGA2xNSGA2';
 
 END;
