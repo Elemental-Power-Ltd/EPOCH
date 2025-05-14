e7dfde66eb7d315b35b60dd57049473b3ce7fb45
William Drouard <william@elementalpower.co.uk>
2025-05-14 11:20:32 +0100

    fix linting

--- data_service/migrations/000008_add_optimisers.up.sql
+++ data_service/migrations/000008_add_optimisers.up.sql
@@ -1,3 +1,7 @@
+BEGIN;
+
 INSERT INTO optimisation.optimisers (name) VALUES ('Bayesian');
 INSERT INTO optimisation.optimisers (name) VALUES ('SeparatedNSGA2');
 INSERT INTO optimisation.optimisers (name) VALUES ('SeparatedNSGA2xNSGA2');
+
+END;
