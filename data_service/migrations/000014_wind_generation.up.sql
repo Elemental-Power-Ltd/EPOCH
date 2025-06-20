48ffb43bca27a8c76cb957ff4f2387102f85d92a
Matt Bailey <32373320+Matt-HJ-Bailey@users.noreply.github.com>
2025-06-20 16:21:17 +0100

    Fixed SQL lint complaints and renumbered

--- data_service/migrations/000014_wind_generation.up.sql
+++ data_service/migrations/000014_wind_generation.up.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 CREATE TABLE renewables.wind (
-    dataset_id UUID REFERENCES renewables.metadata(dataset_id),
+    dataset_id UUID REFERENCES renewables.metadata (dataset_id),
     start_ts TIMESTAMPTZ NOT NULL,
     end_ts TIMESTAMPTZ NOT NULL,
     wind DOUBLE PRECISION NOT NULL
