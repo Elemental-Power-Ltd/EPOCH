dd36add67d8eed0a70f05921a02fc863071412d3
Matt Bailey <32373320+Matt-HJ-Bailey@users.noreply.github.com>
2025-07-23 13:43:07 +0100

    Bumped ruff and mypy requirements, and included migrations

---
+++ data_service/migrations/000016_data_bundles.up.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+CREATE SCHEMA IF NOT EXISTS data_bundles;
+
+CREATE TABLE data_bundles.metadata (
+    bundle_id UUID NOT NULL,
+    name TEXT,
+    site_id TEXT REFERENCES client_info.site_info (site_id),
+    start_ts TIMESTAMPTZ,
+    end_ts TIMESTAMPTZ,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    PRIMARY KEY (bundle_id)
+);
+
+CREATE TABLE data_bundles.dataset_links (
+    bundle_id UUID NOT NULL REFERENCES data_bundles.metadata (bundle_id),
+    dataset_type TEXT NOT NULL,
+    dataset_subtype TEXT,
+    dataset_id UUID NOT NULL,
+    dataset_order INTEGER
+);
+
+END;
