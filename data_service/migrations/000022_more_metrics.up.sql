2501aea03ce3775e2347fb7341bb8afc6179a57c
Jon Warren <156220732+EPJonWarren@users.noreply.github.com>
2025-08-21 12:00:42 +0100

    sqlint: make type uppercase

--- data_service/migrations/000022_more_metrics.up.sql
+++ data_service/migrations/000022_more_metrics.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE TYPE grade_enum AS ENUM ('A', 'B', 'C', 'D', 'E', 'F', 'G');
+CREATE TYPE GRADE_ENUM AS ENUM ('A', 'B', 'C', 'D', 'E', 'F', 'G');
 
 ALTER TABLE optimisation.portfolio_results
 ADD COLUMN metric_total_electricity_used DOUBLE PRECISION,
@@ -25,7 +25,7 @@ ADD COLUMN metric_total_scope_2_emissions DOUBLE PRECISION,
 ADD COLUMN metric_total_combined_carbon_emissions DOUBLE PRECISION,
 ADD COLUMN metric_combined_carbon_balance DOUBLE PRECISION,
 ADD COLUMN metric_scenario_environmental_impact_score DOUBLE PRECISION,
-ADD COLUMN metric_scenario_environmental_impact_grade grade_enum,
+ADD COLUMN metric_scenario_environmental_impact_grade GRADE_ENUM,
 
 ADD COLUMN metric_baseline_electricity_used DOUBLE PRECISION,
 ADD COLUMN metric_baseline_electricity_curtailed DOUBLE PRECISION,
@@ -33,6 +33,6 @@ ADD COLUMN metric_baseline_scope_1_emissions DOUBLE PRECISION,
 ADD COLUMN metric_baseline_scope_2_emissions DOUBLE PRECISION,
 ADD COLUMN metric_baseline_combined_carbon_emissions DOUBLE PRECISION,
 ADD COLUMN metric_baseline_environmental_impact_score DOUBLE PRECISION,
-ADD COLUMN metric_baseline_environmental_impact_grade grade_enum;
+ADD COLUMN metric_baseline_environmental_impact_grade GRADE_ENUM;
 
 END;
