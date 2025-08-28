a8c0e6cd7429bb3f1cbf78916c012fbffa9c2656
William Drouard <william@elementalpower.co.uk>
2025-08-28 13:44:30 +0100

    fix sqlfluff

--- data_service/migrations/000024_optimisation_task_site_table.up.sql
+++ data_service/migrations/000024_optimisation_task_site_table.up.sql
@@ -12,35 +12,35 @@ CREATE TABLE IF NOT EXISTS optimisation.site_task_config (
 INSERT INTO optimisation.site_task_config (task_id, site_id, site_range, site_data, site_constraints)
 WITH pr AS (
     SELECT
-        task_id,
-        key AS site_id,
-        value AS site_range
+        tc.task_id,
+        prj.key AS site_id,
+        prj.value AS site_range
     FROM
-        optimisation.task_config, jsonb_each(portfolio_range)
+        optimisation.task_config AS tc, jsonb_each(tc.portfolio_range) AS prj
     WHERE
-        portfolio_range IS NOT NULL AND jsonb_typeof(portfolio_range) != 'null'
+        tc.portfolio_range IS NOT NULL AND jsonb_typeof(tc.portfolio_range) != 'null'
 ),
 
 sd AS (
     SELECT
-        task_id,
-        key AS site_id,
-        value AS site_data
+        tc.task_id,
+        sdj.key AS site_id,
+        sdj.value AS site_data
     FROM
-        optimisation.task_config, jsonb_each(input_data)
+        optimisation.task_config AS tc, jsonb_each(tc.input_data) AS sdj
     WHERE
-        input_data IS NOT NULL AND jsonb_typeof(input_data) != 'null'
+        tc.input_data IS NOT NULL AND jsonb_typeof(tc.input_data) != 'null'
 ),
 
 sc AS (
     SELECT
-        task_id,
-        key AS site_id,
-        value AS constraints
+        tc.task_id,
+        scj.key AS site_id,
+        scj.value AS constr
     FROM
-        optimisation.task_config, jsonb_each(site_constraints)
+        optimisation.task_config AS tc, jsonb_each(tc.site_constraints) AS scj
     WHERE
-        site_constraints IS NOT NULL AND jsonb_typeof(site_constraints) != 'null'
+        tc.site_constraints IS NOT NULL AND jsonb_typeof(tc.site_constraints) != 'null'
 )
 
 SELECT
@@ -48,11 +48,11 @@ SELECT
     pr.site_id,
     pr.site_range,
     sd.site_data,
-    sc.constraints AS site_constraints
+    sc.constr AS site_constraints
 FROM
     optimisation.task_config AS tc
 LEFT JOIN pr
-    ON pr.site_id = site_id AND tc.task_id = pr.task_id
+    ON tc.task_id = pr.task_id
 LEFT JOIN sd
     ON pr.site_id = sd.site_id AND tc.task_id = sd.task_id
 LEFT JOIN sc
