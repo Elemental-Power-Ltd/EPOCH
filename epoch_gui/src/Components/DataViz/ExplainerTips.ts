b0cf33f23a7418954ae776a82caef1d3c9cefa7a
Jon Warren <156220732+EPJonWarren@users.noreply.github.com>
2025-10-20 16:59:26 +0100

    switch from mp4 -> webm (#185)
    unreal browser can't doesn't have 3rd party codecs for h264, these are using vp8 instead

--- epoch_gui/src/Components/DataViz/ExplainerTips.ts
+++ epoch_gui/src/Components/DataViz/ExplainerTips.ts
@@ -6,7 +6,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "Bars above the line are energy sources coming in to your site, including grid imports (grey), solar generation (yellow) and your battery discharging. Each bar represents one half hour time window, and more yellow means you’re using more of your on-site solar.",
         media: {
             type: "video",
-            src: "/dataviz/explain/energy_sources.mp4"
+            src: "/dataviz/explain/energy_sources.webm"
         }
     },
     {
@@ -14,7 +14,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "Bars below the line are consumers of energy, such as your heat pump (green), your usual electrical load (blue), any electricity you're exporting to the grid (grey) or charging your battery.",
         media: {
             type: "video",
-            src: "/dataviz/explain/energy_consumers.mp4"
+            src: "/dataviz/explain/energy_consumers.webm"
         }
     },
     {
@@ -22,7 +22,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "Batteries will show up both below (charging) and above (discharging) the line in purple, including any inefficiencies in light purple",
         media: {
             type: "video",
-            src: "/dataviz/explain/energy_storage.mp4"
+            src: "/dataviz/explain/energy_storage.webm"
         }
     },
     {
@@ -30,7 +30,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "With everything included, your energy will be perfectly balanced above and below the line, showing where every kWh is coming from and where it's going to over the whole year.",
         media: {
             type: "video",
-            src: "/dataviz/explain/energy_balance.mp4"
+            src: "/dataviz/explain/energy_balance.webm"
         }
     },
     {
@@ -38,7 +38,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "Use the date picker to select the starting date",
         media: {
             type: "video",
-            src: "/dataviz/explain/date_picker.mp4"
+            src: "/dataviz/explain/date_picker.webm"
         }
     },
     {
@@ -46,7 +46,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "Change the duration with the 'Days' dropdown.",
         media: {
             type: "video",
-            src: "/dataviz/explain/time_duration.mp4"
+            src: "/dataviz/explain/time_duration.webm"
         }
     },
     {
@@ -54,7 +54,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "The  ◀ ▶ buttons will scroll your view backwards and forwards one day. The << and >> buttons will scroll your view backwards and forwards one calendar month",
         media: {
             type: "video",
-            src: "/dataviz/explain/time_scrolling.mp4"
+            src: "/dataviz/explain/time_scrolling.webm"
         }
     },
     {
@@ -62,7 +62,7 @@ export const DataVizTips: ExplainerTip[] = [
         text: "Hover over a bar to see what it represents and how much energy was used in that time period.",
         media: {
             type: "video",
-            src: "/dataviz/explain/mouseover.mp4"
+            src: "/dataviz/explain/mouseover.webm"
         }
     },
 ]
