9b7dd2d20b684cb4d08b3b4f168498aef9b27120
William Drouard <william@elementalpower.co.uk>
2026-01-22 11:45:14 +0000

    site range size calc util

--- optimisation_service/app/internal/site_range.py
+++ optimisation_service/app/internal/site_range.py
@@ -55,3 +55,56 @@ def count_parameters_in_asset(asset: dict[str, list[float]]) -> int:
             n += 1
 
     return n
+
+
+def site_range_size(site_range: SiteRange) -> int:
+    """
+    Count the total number of combinations of a site range.
+
+    Parameters
+    ----------
+    site_range
+        Site range to analyse.
+
+    Returns
+    -------
+    n
+        Number of combinations
+    """
+    n = 1
+
+    site_range_dict = site_range.model_dump(exclude_none=True)
+
+    for asset_name, asset in site_range_dict.items():
+        if asset_name in REPEAT_COMPONENTS:
+            for sub_asset in asset:
+                n *= asset_range_size(sub_asset)
+        else:
+            n *= asset_range_size(asset)
+
+    return n
+
+
+def asset_range_size(asset: dict[str, list[float]]) -> int:
+    """
+    Count the number of combinations of an individual asset.
+
+    Parameters
+    ----------
+    asset
+        An individual component within the SiteRange.
+
+    Returns
+    -------
+    n
+        Number of combinations
+    """
+    n = 1
+    if not asset["COMPONENT_IS_MANDATORY"]:
+        n *= 2
+    for attribute in asset.values():
+        # each list in the asset is a varying parameter
+        if isinstance(attribute, list):
+            n *= len(attribute)
+
+    return n
